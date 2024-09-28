f0feac004c3ac81266ef09080eb9215d09862aa5
qqdzv <145340990+qqdzv@users.noreply.github.com>
2024-09-28 14:37:16 +0300

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,11 @@
+import sys
+from loguru import logger
+
+def logging_setup():
+    format_info = "<green>{time:HH:mm:ss.SS}</green> | <blue>{level}</blue> | <level>{message}</level>"
+    logger.remove()
+
+    logger.add(sys.stdout, colorize=True, format=format_info, level="INFO")
+    logger.add("hack_1.log", rotation="50 MB", compression="zip", format=format_info, level="TRACE")
+    
+logging_setup()
